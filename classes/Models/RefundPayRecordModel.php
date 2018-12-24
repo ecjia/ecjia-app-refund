135dde40d39db60b66fbd11bf2970a76a9bb2478
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-12-24 17:43:09 +0800

    添加退款表数据模型

---
+++ classes/Models/RefundPayRecordModel.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2018/12/24
+ * Time: 17:15
+ */
+namespace Ecjia\App\Refund\Models;
+
+class RefundPayRecordModel
+{
+    protected $table = 'refund_payrecord';
+
+    protected $primaryKey = 'id';
+
+    /**
+     * 可以被批量赋值的属性。
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'store_id',
+        'order_id',
+        'order_sn',
+        'refund_id',
+        'refund_sn',
+        'refund_type',
+        'goods_amount',
+        'order_money_paid',
+        'payment_record_id',
+        'back_pay_code',
+        'back_pay_name',
+        'back_pay_fee',
+        'back_shipping_fee',
+        'back_insure_fee',
+        'back_pack_id',
+        'back_pack_fee',
+        'back_card_id',
+        'back_card_fee',
+        'back_bonus_id',
+        'back_bonus',
+        'back_surplus',
+        'back_integral',
+        'back_integral_money',
+        'back_inv_tax',
+        'back_money_total',
+        'add_time',
+        'action_back_content',
+        'action_back_type',
+        'action_back_time',
+        'action_user_type',
+        'action_user_id',
+        'action_user_name',
+    ];
+
+    /**
+     * 该模型是否被自动维护时间戳
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+
+    /**
+     * 获取与此打款记录关联的退款订单
+     */
+    public function refundOrder()
+    {
+        return $this->belongsTo('Ecjia\App\Refund\Models\RefundOrderModel', 'refund_id', 'refund_id');
+    }
+
+}
