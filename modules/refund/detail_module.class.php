9b8edfbc70fd3764bd9ec39e6eadcafe57c1943e
周瑞丽 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-10 13:22:06 +0800

    订单详情，退款详情日志状态

--- modules/refund/detail_module.class.php
+++ modules/refund/detail_module.class.php
@@ -236,14 +236,15 @@ class refund_detail_module extends api_front implements api_interface {
 		$logs = array();
 		if (!empty($refund_logs)) {
 			$labe_order_status = [
-				'apply'                => __('申请退款', 'refund'),
-				'agree'          	   => __('服务单退款申请已处理', 'refund'), //下单
-				'refunded'             => __('退款到账', 'refund'),
+				'refund_apply'         => __('申请退款', 'refund'),
+				'refund_agree'         => __('服务单退款申请已处理', 'refund'), 
+				'refund_success'       => __('退款到账', 'refund'),
 				'confirm_received'     => __('确认收货处理', 'refund'),
 				'return_finished'      => __('订单退货退款已处理', 'refund'),
 				'refund_canceled'	   => __('撤销退款申请', 'refund'),				   
 				'merchant_unconfirmed' => __('无法接单', 'refund'),
 				'returned_goods'	   => __('返还退货商品', 'refund'),
+				'return_agree'         => __('订单退货退款申请已处理', 'refund'), //下单
 			];
 			foreach ($refund_logs as $log) {
 				$logs[] = array(
